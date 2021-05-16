a3171025dc425a9ff03e0b850e02b1f45d47e092
Felix (xq) Queißner <git@mq32.de>
2021-05-16 22:01:11 +0200

    Adds basic texture support.

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,9 @@ const RequestedExtensions = struct {
     KHR_debug: bool,
 };
 
+// opengl docs can be found here:
+// https://www.khronos.org/registry/OpenGL-Refpages/es2.0/
+
 pub fn main() anyerror!void {
     _ = c.SDL_Init(c.SDL_INIT_EVERYTHING);
     defer _ = c.SDL_Quit();
@@ -90,6 +93,13 @@ pub fn main() anyerror!void {
     var screen_width: u15 = 0;
     var screen_height: u15 = 0;
 
+    const texture_handle = try renderer.createTexture(2, 2, &[2 * 2 * 4]u8{
+        0xFF, 0x00, 0x00, 0xFF,
+        0x00, 0xFF, 0x00, 0xFF,
+        0x00, 0x00, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF,
+    });
+
     main_loop: while (true) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
@@ -147,6 +157,15 @@ pub fn main() anyerror!void {
             try renderer.fillRectangle(34, 67, 32, 32, red);
             try renderer.drawLine(34, 67, 65, 67, white);
             try renderer.drawLine(34, 98, 65, 98, white);
+
+            try renderer.fillTexturedRectangle(
+                (screen_width - 256) / 2,
+                (screen_height - 256) / 2,
+                256,
+                256,
+                texture_handle,
+                null,
+            );
         }
 
         {
@@ -212,10 +231,12 @@ pub fn main() anyerror!void {
 const Renderer = struct {
     const Self = @This();
 
-    const Vertex = extern struct {
+    pub const Vertex = extern struct {
         // coordinates on the screen in pixels:
         x: f32,
         y: f32,
+        u: f32,
+        v: f32,
         // color of the vertex:
         r: u8,
         g: u8,
@@ -227,6 +248,8 @@ const Renderer = struct {
             return .{
                 .x = @intToFloat(f32, x),
                 .y = @intToFloat(f32, y),
+                .u = 0,
+                .v = 0,
                 .r = color.r,
                 .g = color.g,
                 .b = color.b,
@@ -241,47 +264,99 @@ const Renderer = struct {
             v.y += 0.5 * dy;
             return v;
         }
+
+        /// changes the UV coordinates of the vertex.
+        fn withUV(self: @This(), u: f32, v: f32) @This() {
+            var vert = self;
+            vert.u = u;
+            vert.v = v;
+            return vert;
+        }
     };
+
+    pub const Texture = struct {
+        /// private texture handle
+        handle: gles.GLuint,
+
+        /// private reference counter.
+        /// This is required as texture references are held in the internal draw 
+        /// queue when passing them into a draw command and will be released after
+        /// the `render()` call
+        refcount: usize,
+
+        /// width of the texture in pixels
+        width: u15,
+
+        /// height of the texture in pixels
+        height: u15,
+    };
+
+    const DrawCall = struct {
+        offset: usize,
+        count: usize,
+        texture: ?*const Texture,
+    };
+
+    const TextureList = std.TailQueue(Texture);
+    const TextureItem = std.TailQueue(Texture).Node;
+
+    const CollectError = error{OutOfMemory};
+    const CreateTextureError = error{ OutOfMemory, GraphicsApiFailure };
+    const InitError = error{ OutOfMemory, GraphicsApiFailure };
 
     const vertexSource =
         \\attribute vec2 vPosition;
         \\attribute vec4 vColor;
+        \\attribute vec2 vUV;
         \\uniform ivec2 uScreenSize;
         \\varying vec4 fColor;
+        \\varying vec2 fUV;
         \\void main()
         \\{
-        \\   vec2 virtual_position = (vPosition + 0.375) / vec2(uScreenSize);
+        \\   vec2 virtual_position = (vPosition + 0.5) / vec2(uScreenSize);
         \\   gl_Position = vec4(2.0 * virtual_position.x - 1.0, 1.0 - 2.0 * virtual_position.y, 0.0, 1.0);
         \\   fColor = vColor;
+        \\   fUV = vUV;
         \\}
     ;
     const fragmentSource =
         \\precision mediump float;
         \\varying vec4 fColor;
+        \\varying vec2 fUV;
+        \\uniform float uTextureEnable;
+        \\uniform sampler2D uTexture;
         \\void main()
         \\{
-        \\  gl_FragColor = fColor;
+        \\   vec4 base_color = mix(vec4(1), texture2D(uTexture, fUV), uTextureEnable);
+        \\   gl_FragColor = fColor * base_color;
         \\}
     ;
 
     shader_program: gles.GLuint,
     screen_size_location: gles.GLint,
+    texture_enable_location: gles.GLint,
+    texture_location: gles.GLint,
 
     vertex_buffer: gles.GLuint,
 
     position_attribute_location: gles.GLuint,
     color_attribute_location: gles.GLuint,
+    uv_attribute_location: gles.GLuint,
 
     /// list of CCW triangles that will be rendered 
     vertices: std.ArrayList(Vertex),
+    draw_calls: std.ArrayList(DrawCall),
 
-    pub fn init(allocator: *std.mem.Allocator) !Self {
+    allocator: *std.mem.Allocator,
+    textures: TextureList,
+
+    pub fn init(allocator: *std.mem.Allocator) InitError!Self {
         const shader_program = blk: {
             // Create and compile vertex shader
-            const vertex_shader = try createAndCompileShader(gles.VERTEX_SHADER, vertexSource);
+            const vertex_shader = createAndCompileShader(gles.VERTEX_SHADER, vertexSource) catch return error.GraphicsApiFailure;
             defer gles.deleteShader(vertex_shader);
 
-            const fragment_shader = try createAndCompileShader(gles.FRAGMENT_SHADER, fragmentSource);
+            const fragment_shader = createAndCompileShader(gles.FRAGMENT_SHADER, fragmentSource) catch return error.GraphicsApiFailure;
             defer gles.deleteShader(fragment_shader);
 
             const program = gles.createProgram();
@@ -297,7 +372,7 @@ const Renderer = struct {
             var status: gles.GLint = undefined;
             gles.getProgramiv(program, gles.LINK_STATUS, &status);
             if (status != gles.TRUE)
-                return error.FailedToLinkProgram;
+                return error.GraphicsApiFailure;
 
             break :blk program;
         };
@@ -306,37 +381,126 @@ const Renderer = struct {
         // Create vertex buffer object and copy vertex data into it
         var vertex_buffer: gles.GLuint = 0;
         gles.genBuffers(1, &vertex_buffer);
+        if (vertex_buffer == 0)
+            return error.GraphicsApiFailure;
         errdefer gles.deleteBuffers(1, &vertex_buffer);
 
-        const position_attribute_location = std.math.cast(gles.GLuint, gles.getAttribLocation(shader_program, "vPosition")) catch return error.MissingShaderAttribute;
-        const color_attribute_location = std.math.cast(gles.GLuint, gles.getAttribLocation(shader_program, "vColor")) catch return error.MissingShaderAttribute;
+        const position_attribute_location = std.math.cast(gles.GLuint, gles.getAttribLocation(shader_program, "vPosition")) catch return error.GraphicsApiFailure;
+        const color_attribute_location = std.math.cast(gles.GLuint, gles.getAttribLocation(shader_program, "vColor")) catch return error.GraphicsApiFailure;
+        const uv_attribute_location = std.math.cast(gles.GLuint, gles.getAttribLocation(shader_program, "vUV")) catch return error.GraphicsApiFailure;
 
         gles.enableVertexAttribArray(position_attribute_location);
         gles.enableVertexAttribArray(color_attribute_location);
+        gles.enableVertexAttribArray(uv_attribute_location);
 
         gles.bindBuffer(gles.ARRAY_BUFFER, vertex_buffer);
         gles.vertexAttribPointer(position_attribute_location, 2, gles.FLOAT, gles.FALSE, @sizeOf(Vertex), @intToPtr(?*const c_void, @byteOffsetOf(Vertex, "x")));
         gles.vertexAttribPointer(color_attribute_location, 4, gles.UNSIGNED_BYTE, gles.TRUE, @sizeOf(Vertex), @intToPtr(?*const c_void, @byteOffsetOf(Vertex, "r")));
+        gles.vertexAttribPointer(uv_attribute_location, 2, gles.FLOAT, gles.FALSE, @sizeOf(Vertex), @intToPtr(?*const c_void, @byteOffsetOf(Vertex, "u")));
 
         return Self{
             .shader_program = shader_program,
             .screen_size_location = gles.getUniformLocation(shader_program, "uScreenSize"),
+            .texture_enable_location = gles.getUniformLocation(shader_program, "uTextureEnable"),
+            .texture_location = gles.getUniformLocation(shader_program, "uTexture"),
             .vertices = std.ArrayList(Vertex).init(allocator),
             .vertex_buffer = vertex_buffer,
             .position_attribute_location = position_attribute_location,
             .color_attribute_location = color_attribute_location,
+            .uv_attribute_location = uv_attribute_location,
+            .allocator = allocator,
+            .textures = .{},
+            .draw_calls = std.ArrayList(DrawCall).init(allocator),
         };
     }
 
     pub fn deinit(self: *Self) void {
+        while (self.textures.first) |tex| {
+            self.destroyTextureInternal(tex);
+        }
         gles.deleteBuffers(1, &self.vertex_buffer);
         gles.deleteProgram(self.shader_program);
+        self.draw_calls.deinit();
         self.vertices.deinit();
         self.* = undefined;
     }
 
+    /// Creates a new texture for this renderer with the size `width`×`height`.
+    /// The texture is only valid as long as the renderer is valid *or* `destroyTexture` is called,
+    /// whichever happens first.
+    /// If `initial_data` is given, the data is encoded as BGRA pixels.
+    pub fn createTexture(self: *Self, width: u15, height: u15, initial_data: ?[]const u8) !*const Texture {
+        var id: gles.GLuint = undefined;
+        gles.genTextures(1, &id);
+        if (id == 0)
+            return error.GraphicsApiFailure;
+
+        gles.bindTexture(gles.TEXTURE_2D, id);
+        if (initial_data) |data| {
+            std.debug.assert(data.len == 4 * @as(usize, width) * @as(usize, height));
+            gles.texImage2D(gles.TEXTURE_2D, 0, gles.RGBA, width, height, 0, gles.RGBA, gles.UNSIGNED_BYTE, data.ptr);
+        } else {
+            gles.texImage2D(gles.TEXTURE_2D, 0, gles.RGBA, width, height, 0, gles.RGBA, gles.UNSIGNED_BYTE, null);
+        }
+        gles.texParameteri(gles.TEXTURE_2D, gles.TEXTURE_MIN_FILTER, gles.LINEAR);
+        gles.texParameteri(gles.TEXTURE_2D, gles.TEXTURE_MAG_FILTER, gles.LINEAR);
+
+        const tex_node = try self.allocator.create(TextureItem);
+        tex_node.* = .{
+            .data = Texture{
+                .handle = id,
+                .refcount = 1,
+                .width = width,
+                .height = height,
+            },
+        };
+        self.textures.append(tex_node);
+        return &tex_node.data;
+    }
+
+    fn makeTextureMut(ptr: *const Texture) *Texture {
+        return @intToPtr(*Texture, @ptrToInt(ptr));
+    }
+
+    /// Destroys a texture and releases all of its memory.
+    /// The texture passed here must be created with `createTexture`.
+    pub fn destroyTexture(self: *Self, texture: *const Texture) void {
+        // we can do this as texture handles are only given out via `createTexture` which
+        // returns a immutable reference.
+        const mut_texture = makeTextureMut(texture);
+        const node = @fieldParentPtr(TextureItem, "data", mut_texture);
+        destroyTextureInternal(self, node);
+    }
+
+    fn destroyTextureInternal(self: *Self, node: *TextureItem) void {
+        node.data.refcount -= 1;
+        if (node.data.refcount > 0)
+            return;
+
+        self.textures.remove(node);
+
+        gles.deleteTextures(1, &node.data.handle);
+        node.* = undefined;
+
+        self.allocator.destroy(node);
+    }
+
+    /// Updates the texture data of the given texture.
+    /// `data` is encoded as BGRA pixels.
+    pub fn updateTexture(self: *Self, texture: *Texture, data: []const u8) void {
+        std.debug.assert(data.len == 4 * @as(usize, texture.width) * @as(usize, texture.height));
+        gles.bindTexture(gles.TEXTURE_2D, texture.handle);
+        gles.texImage2D(gles.TEXTURE_2D, 0, gles.RGBA, width, height, 0, gles.RGBA, gles.UNSIGNED_BYTE, data.ptr);
+    }
+
     /// Resets the state of the renderer and prepares a fresh new frame.
     pub fn reset(self: *Self) void {
+        for (self.draw_calls.items) |draw_call| {
+            if (draw_call.texture) |tex| {
+                self.destroyTexture(tex);
+            }
+        }
+        self.draw_calls.shrinkRetainingCapacity(0);
         self.vertices.shrinkRetainingCapacity(0);
     }
 
@@ -347,52 +511,108 @@ const Renderer = struct {
 
         gles.useProgram(self.shader_program);
         gles.uniform2i(self.screen_size_location, width, height);
+        gles.uniform1i(self.texture_location, 0);
 
         gles.enable(gles.BLEND);
         gles.blendFunc(gles.SRC_ALPHA, gles.ONE_MINUS_SRC_ALPHA);
         gles.blendEquation(gles.FUNC_ADD);
 
-        gles.drawArrays(gles.TRIANGLES, 0, @intCast(gles.GLsizei, self.vertices.items.len));
+        gles.activeTexture(gles.TEXTURE0);
+        for (self.draw_calls.items) |draw_call| {
+            if (draw_call.texture) |tex_ptr| {
+                gles.bindTexture(gles.TEXTURE_2D, tex_ptr.handle);
+                gles.uniform1f(self.texture_enable_location, 1);
+            } else {
+                gles.bindTexture(gles.TEXTURE_2D, 0);
+                gles.uniform1f(self.texture_enable_location, 0);
+            }
+
+            gles.drawArrays(
+                gles.TRIANGLES,
+                @intCast(gles.GLsizei, draw_call.offset),
+                @intCast(gles.GLsizei, draw_call.count),
+            );
+        }
+        gles.bindTexture(gles.TEXTURE_2D, 0);
+    }
+
+    /// Appends a set of triangles to the renderer with the given `texture`. 
+    pub fn appendTriangles(self: *Self, texture: ?*const Texture, triangles: []const [3]Vertex) !void {
+        const draw_call = if (self.draw_calls.items.len == 0 or self.draw_calls.items[self.draw_calls.items.len - 1].texture != texture) blk: {
+            const dc = try self.draw_calls.addOne();
+            dc.* = DrawCall{
+                .texture = texture,
+                .offset = self.vertices.items.len,
+                .count = 0,
+            };
+            if (texture) |tex_ptr| {
+                makeTextureMut(tex_ptr).refcount += 1;
+            }
+            break :blk dc;
+        } else &self.draw_calls.items[self.draw_calls.items.len - 1];
+
+        std.debug.assert(draw_call.texture == texture);
+
+        try self.vertices.ensureCapacity(self.vertices.items.len + 3 * triangles.len);
+        for (triangles) |tris| {
+            try self.vertices.appendSlice(&tris);
+        }
+
+        draw_call.count += 3 * triangles.len;
     }
 
     /// Appends a filled, untextured quad.
-    pub fn fillRectangle(self: *Self, x: i16, y: i16, width: u15, height: u15, color: Color) !void {
-        const tl = Vertex.init(x, y, color);
-        const tr = Vertex.init(x + width, y, color);
-        const bl = Vertex.init(x, y + height, color);
-        const br = Vertex.init(x + width, y + height, color);
+    pub fn fillRectangle(self: *Self, x: i16, y: i16, width: u15, height: u15, color: Color) CollectError!void {
+        const tl = Vertex.init(x, y, color).offset(-1, -1);
+        const tr = Vertex.init(x + width - 1, y, color).offset(1, -1);
+        const bl = Vertex.init(x, y + height - 1, color).offset(-1, 1);
+        const br = Vertex.init(x + width - 1, y + height - 1, color).offset(1, 1);
 
-        try self.vertices.appendSlice(&[_]Vertex{
-            tl, br, tr,
-            br, tl, bl,
+        try self.appendTriangles(null, &[_][3]Vertex{
+            .{ tl, br, tr },
+            .{ br, tl, bl },
+        });
+    }
+
+    /// Copies the given texture to the screen
+    pub fn fillTexturedRectangle(self: *Self, x: i16, y: i16, width: u15, height: u15, texture: *const Texture, tint: ?Color) CollectError!void {
+        const color = tint orelse Color{ .r = 0xFF, .g = 0xFF, .b = 0xFF, .a = 0xFF };
+        const tl = Vertex.init(x, y, color).offset(-1, -1).withUV(0, 0);
+        const tr = Vertex.init(x + width - 1, y, color).offset(1, -1).withUV(1, 0);
+        const bl = Vertex.init(x, y + height - 1, color).offset(-1, 1).withUV(0, 1);
+        const br = Vertex.init(x + width - 1, y + height - 1, color).offset(1, 1).withUV(1, 1);
+
+        try self.appendTriangles(texture, &[_][3]Vertex{
+            .{ tl, br, tr },
+            .{ br, tl, bl },
         });
     }
 
     /// Appends a rectangle with a 1 pixel wide outline
-    pub fn drawRectangle(self: *Self, x: i16, y: i16, width: u15, height: u15, color: Color) !void {
+    pub fn drawRectangle(self: *Self, x: i16, y: i16, width: u15, height: u15, color: Color) CollectError!void {
         const tl = Vertex.init(x, y, color);
         const tr = Vertex.init(x + width - 1, y, color);
         const bl = Vertex.init(x, y + height - 1, color);
         const br = Vertex.init(x + width - 1, y + height - 1, color);
 
-        try self.vertices.appendSlice(&[_]Vertex{
+        try self.appendTriangles(null, &[_][3]Vertex{
             // top
-            tl.offset(-1, -1), tr.offset(-1, 1),  tr.offset(1, -1),
-            tr.offset(-1, 1),  tl.offset(-1, -1), tl.offset(1, 1),
+            .{ tl.offset(-1, -1), tr.offset(-1, 1), tr.offset(1, -1) },
+            .{ tr.offset(-1, 1), tl.offset(-1, -1), tl.offset(1, 1) },
             // right
-            tr.offset(1, -1),  tr.offset(-1, 1),  br.offset(-1, -1),
-            br.offset(-1, -1), br.offset(1, 1),   tr.offset(1, -1),
+            .{ tr.offset(1, -1), tr.offset(-1, 1), br.offset(-1, -1) },
+            .{ br.offset(-1, -1), br.offset(1, 1), tr.offset(1, -1) },
             // bottom
-            br.offset(-1, -1), bl.offset(-1, 1),  br.offset(1, 1),
-            br.offset(-1, -1), bl.offset(1, -1),  bl.offset(-1, 1),
+            .{ br.offset(-1, -1), bl.offset(-1, 1), br.offset(1, 1) },
+            .{ br.offset(-1, -1), bl.offset(1, -1), bl.offset(-1, 1) },
             // left
-            bl.offset(1, -1),  tl.offset(-1, -1), bl.offset(-1, 1),
-            bl.offset(1, 1),   tl.offset(1, 1),   tl.offset(-1, -1),
+            .{ bl.offset(1, -1), tl.offset(-1, -1), bl.offset(-1, 1) },
+            .{ bl.offset(1, 1), tl.offset(1, 1), tl.offset(-1, -1) },
         });
     }
 
     /// Draws a single pixel wide line from (`x0`,`y0`) to (`x1`,`y1`)
-    pub fn drawLine(self: *Self, x0: i16, y0: i16, x1: i16, y1: i16, color: Color) !void {
+    pub fn drawLine(self: *Self, x0: i16, y0: i16, x1: i16, y1: i16, color: Color) CollectError!void {
         const p0 = Vertex.init(x0, y0, color);
         const p1 = Vertex.init(x1, y1, color);
 
@@ -407,14 +627,17 @@ const Renderer = struct {
         const oy_x = ox_y;
         const oy_y = -ox_x;
 
-        try self.vertices.appendSlice(&[_]Vertex{
-            p0.offset(-ox_x, -ox_y).offset(-oy_x, -oy_y),
-            p1.offset(ox_x, ox_y).offset(-oy_x, -oy_y),
-            p1.offset(ox_x, ox_y).offset(oy_x, oy_y),
-
-            p0.offset(-ox_x, -ox_y).offset(-oy_x, -oy_y),
-            p1.offset(ox_x, ox_y).offset(oy_x, oy_y),
-            p0.offset(-ox_x, -ox_y).offset(oy_x, oy_y),
+        try self.appendTriangles(null, &[_][3]Vertex{
+            .{
+                p0.offset(-ox_x, -ox_y).offset(-oy_x, -oy_y),
+                p1.offset(ox_x, ox_y).offset(-oy_x, -oy_y),
+                p1.offset(ox_x, ox_y).offset(oy_x, oy_y),
+            },
+            .{
+                p0.offset(-ox_x, -ox_y).offset(-oy_x, -oy_y),
+                p1.offset(ox_x, ox_y).offset(oy_x, oy_y),
+                p0.offset(-ox_x, -ox_y).offset(oy_x, oy_y),
+            },
         });
     }
 
